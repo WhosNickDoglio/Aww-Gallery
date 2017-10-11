4351df9e84dedf67358c252a4a9f6348101875ea
Nicholas <NicholasDoglio@Gmail.com>
2017-10-11 07:17:43 -0400

    List teardown

--- app/src/main/java/com/nicholasdoglio/eyebleach/ui/photolist/PhotoGridAdapter.java
+++ app/src/main/java/com/nicholasdoglio/eyebleach/ui/photolist/PhotoGridAdapter.java
@@ -17,88 +17,9 @@
  */
 package com.nicholasdoglio.eyebleach.ui.photolist;
 
-import android.arch.paging.PagedListAdapter;
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.recyclerview.extensions.DiffCallback;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.nicholasdoglio.eyebleach.R;
-import com.nicholasdoglio.eyebleach.data.model.reddit.ChildData;
-import com.nicholasdoglio.eyebleach.util.Intents;
-
-import java.util.Objects;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 /**
  * @author Nicholas Doglio
  */
-public class PhotoGridAdapter extends PagedListAdapter<ChildData, PhotoGridAdapter.PhotoGridViewHolder> {
+public class PhotoGridAdapter {
 
-    private static final DiffCallback<ChildData> DIFF_CALLBACK = new DiffCallback<ChildData>() {
-        @Override
-        public boolean areItemsTheSame(@NonNull ChildData oldItem, @NonNull ChildData newItem) {
-            return Objects.equals(oldItem.getId(), newItem.getId());
-        }
-
-        @Override
-        public boolean areContentsTheSame(@NonNull ChildData oldItem, @NonNull ChildData newItem) {
-            return oldItem.equals(newItem);
-        }
-    };
-    private Context photoGridContext;
-
-    PhotoGridAdapter(Context photoGridContext) {
-        super(DIFF_CALLBACK);
-        this.photoGridContext = photoGridContext;
-    }
-
-    @Override
-    public PhotoGridViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View regularView = LayoutInflater.from(parent.getContext()).inflate(R.layout.photo_grid_item, parent, false);
-        return new PhotoGridViewHolder(regularView);
-    }
-
-    @Override
-    public void onBindViewHolder(PhotoGridViewHolder holder, int position) {
-        ChildData childData = getItem(position);
-
-        if (childData != null) {
-            holder.bindTo(childData);
-        }
-    }
-
-    class PhotoGridViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        @BindView(R.id.image_grid)
-        ImageView photoGridThumbnailImageView;
-
-        PhotoGridViewHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            ButterKnife.bind(this, itemView);
-        }
-
-        void bindTo(ChildData childData) {
-            RequestOptions options = new RequestOptions()
-                    .error(R.drawable.cat_error);
-
-            Glide.with(photoGridContext)
-                    .load(childData.getThumbnail())
-                    .apply(options)
-                    .into(photoGridThumbnailImageView);
-        }
-
-        @Override
-        public void onClick(View view) {
-            Intents.startDetailActivity(view, getAdapterPosition());
-        }
-    }
 }

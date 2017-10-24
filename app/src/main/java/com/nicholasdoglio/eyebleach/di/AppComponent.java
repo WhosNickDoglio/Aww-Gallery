d8f3933b10db4cfa56fcb20600e1e7ddb48ff7a3
Nicholas <NicholasDoglio@Gmail.com>
2017-10-24 17:35:20 -0400

    Cleaned up Dagger a bit

--- app/src/main/java/com/nicholasdoglio/eyebleach/di/AppComponent.java
+++ app/src/main/java/com/nicholasdoglio/eyebleach/di/AppComponent.java
@@ -25,30 +25,25 @@ import javax.inject.Singleton;
 
 import dagger.BindsInstance;
 import dagger.Component;
-import dagger.android.AndroidInjector;
-import dagger.android.DaggerApplication;
-import dagger.android.support.AndroidSupportInjectionModule;
+import dagger.android.AndroidInjectionModule;
 
 /**
  * @author Nicholas Doglio
  */
 @Singleton
 @Component(modules = {
-        AndroidSupportInjectionModule.class,
+        AndroidInjectionModule.class,
         AppModule.class,
         ActivityBindingModule.class,})
-public interface AppComponent extends AndroidInjector<DaggerApplication> {
+public interface AppComponent {
 
     void inject(AwwGalleryApp application);
-
-    @Override
-    void inject(DaggerApplication instance);
 
     @Component.Builder
     interface Builder {
 
         @BindsInstance
-        AppComponent.Builder application(Application application);
+        Builder application(Application application);
 
         AppComponent build();
     }

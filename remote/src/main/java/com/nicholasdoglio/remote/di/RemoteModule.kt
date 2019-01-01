9698f60637c7f3c15dddb3933ee26e5753ea8512
Nicholas Doglio <NicholasDoglio@Gmail.com>
2019-01-01 16:31:38 -0500

    Cleaned up RemoteModule

--- remote/src/main/java/com/nicholasdoglio/remote/di/RemoteModule.kt
+++ remote/src/main/java/com/nicholasdoglio/remote/di/RemoteModule.kt
@@ -1,35 +1,38 @@
 package com.nicholasdoglio.remote.di
 
 import com.nicholasdoglio.remote.RedditService
+import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
+@Module
 object RemoteModule {
 
   private const val BASE_URL = "https://www.reddit.com/"
 
   @Provides
-  @JvmStatic fun interceptor(): HttpLoggingInterceptor =
+  @JvmStatic internal fun interceptor(): HttpLoggingInterceptor =
     HttpLoggingInterceptor().apply {
       level = HttpLoggingInterceptor.Level.BODY
     }
 
   @Provides
-  @JvmStatic fun okHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient =
+  @JvmStatic internal fun okHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient =
     OkHttpClient.Builder().addInterceptor(interceptor).build()
 
-  @Provides @Singleton
-  @JvmStatic fun schoolService(okHttpClient: OkHttpClient): RedditService =
+  @Provides @Singleton //Looking into using JRAW instead
+  @JvmStatic internal fun redditService(okHttpClient: OkHttpClient): RedditService =
     Retrofit.Builder()
       .baseUrl(BASE_URL)
       .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
       .addConverterFactory(MoshiConverterFactory.create())
       .client(okHttpClient)
       .build()
-      .create(RedditService::class.java)
+      .create()
 }
